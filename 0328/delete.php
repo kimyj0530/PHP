ade7e9fe1f240f6759db043b48b11a1b6e99c7ff
김영준 <102145868+kimyj0530@users.noreply.github.com>
2023-03-28 13:57:15 +0900

    Add files via upload

---
+++ 0328/delete.php
@@ -0,0 +1,13 @@
+<?php
+    echo $_GET['num'];
+    $conn = mysqli_connect('localhost', 'root', '','example1');
+    $query = "delete from table1 where num=" . $_GET['num'];
+    $result = mysqli_query($conn, $query);  
+    // 실행 결과 확인
+    if($result){
+        echo "성공";
+    }else{
+        echo "실패";
+    }
+?>
+<meta http-equiv="refresh" content="0; url=input.php">
